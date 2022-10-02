ef207ae32af55ba6058e713440b2122acdf38b58
Ben Gordon <benjamintheepic@gmail.com>
2022-10-02 10:53:59 -0400

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,9 @@ function App() {
   const [homeAddress, setHomeAddress] = useState("")
   const [footprint, setFootprint] = useState(0)
   const [graphData, setGraphData] = useState({maxVal: 2.4, list: [
-    [{from: "home", to: "store", carbon: 2.4}], [], [], [], [], [], []]
+    [{from: "home", to: "store", carbon: 2.4, runSum: 0}], [], [], [], [], [], []]
   })
+  const [hoverBar, setHoverBar] = useState({day: -1, boxIndex: -1})
 
   function handleFakeData() {
     const newData = [[], [], [], [], [], [], []];
@@ -61,17 +62,23 @@ function App() {
   async function getFootprint() {
     const results = [];
     let max = 0;
+    let weekSum = 0;
     for (const day of data) {
       const result = [];
+      let daySum = 0;
       for (let i = 1; i < day.length; i++) {
         const res = await fetch(`http://localhost:3001/driving?origin=${day[i - 1].location}&destination=${day[i].location}`);
         const json = await res.json();
-        max = Math.max(max, json);
-        result.push({from: day[i - 1].location, to: day[i].location, carbon: json});
+        result.push({from: day[i - 1].location, to: day[i].location, carbon: json, runSum: daySum});
+        daySum += json;
+
       }
+      max = Math.max(max, daySum);
+      weekSum += daySum;
       results.push(result);
     }
-    console.log(results);
+    setGraphData({maxVal: max, list: results})
+    setFootprint(weekSum)
   }
 
   return (
@@ -110,15 +117,33 @@ function App() {
       <h3>{homeAddress && `Home Address: ${homeAddress}`}</h3>
 
       <button onClick={() => getFootprint()}>Get carbon footprint</button>
-      <h3>Carbon Footprint: {footprint} kg</h3>
+      <h3>Weekly Carbon Footprint: {footprint} kg</h3>
       {graphData.maxVal > 0 && <svg version="1.1" height="500" width="700" xmlns="http://www.w3.org/2000/svg">
         <rect x="0" y="0" width="100%" height="100%" style={{fill: "#ddd"}}></rect>
         <text x="50%" y="20" dominantBaseline="middle" style={{fontSize: 20, fontWeight: "bold"}} textAnchor="middle">Emissions by day</text>
         {weekDays.map((day, i) => <text y="485" x={`${3 + i*14.28}%`}>{day}</text>)}
         {graphData.list.map((day, i) => <g>
-          {day.map((l, j) => <rect fill="blue" width="11%" y="35" x={`${1.5+ i*14.28}%`} height={l.carbon/graphData.maxVal*425}></rect>)}
-        </g>)}{/*<rect fill="red" opacity=".5" y="35" x={`${1.5+ i*14.28}%`} height="425" width={`11%`}>*/}
-
+          {day.map((l, j) => <rect 
+            className="graphBar" fill="blue" width="11%" stroke="#009" 
+            strokeWidth={2} y={460 - (l.runSum + l.carbon)/graphData.maxVal*425} 
+            x={`${1.5+ i*14.28}%`} height={l.carbon/graphData.maxVal*425}
+            onMouseEnter={() => setHoverBar({day: i, boxIndex: j})}
+            onMouseLeave={() => setHoverBar({day: -1, boxIndex: -1})}>
+          </rect>)}
+        </g>)}
+        {hoverBar.day >= 0 && hoverBar.boxIndex >= 0 && <g><rect 
+          x={20} y={30} width={200} fill={"#ccc"} height={50}>
+          </rect>
+          <text x={20} y={45}>
+            From {graphData.list[hoverBar.day][hoverBar.boxIndex].from}
+          </text>
+          <text x={20} y={60} width={200}>
+            to {graphData.list[hoverBar.day][hoverBar.boxIndex].to}
+          </text>
+          <text x={20} y={75} width={200}>
+            {graphData.list[hoverBar.day][hoverBar.boxIndex].carbon} kg
+          </text>
+        </g>}
       </svg>}
     </div>
   );
